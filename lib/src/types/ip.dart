03ce9ef1bb7ece7c1e39d1d5ab6e3e0c18847237
Danielle Smith <code@danini.dev>
2024-12-31 15:39:35 +0200

    remove dart:io dependency

--- lib/src/types/ip.dart
+++ lib/src/types/ip.dart
@@ -1,19 +1,32 @@
-import "dart:io";
-import "../document.dart";
+import "package:kdl/src/document.dart";
+import "package:kdl/src/exception.dart";
 
-class KdlIP extends KdlValue<InternetAddress> {
-  KdlIP(InternetAddress value, [String? type]) : super(value, type);
+class KdlIP extends KdlValue<String> {
+  KdlIP(String value, [String? type]) : super(value, type);
 }
 
 class KdlIPV4 extends KdlIP {
-  KdlIPV4(InternetAddress value, [String? type]) : super(value, type);
+  static final _DECBYTE = r"""
+    0
+    |1(?:[0-9][0-9]?)?
+    |2(?:[0-4][0-9]?|5[0-5]?|[6-9])?
+    |[3-9][0-9]?
+  """
+      .trim()
+      .split('\n')
+      .map((v) => v.trim())
+      .join();
+  static final PATTERN =
+      RegExp("^($_DECBYTE)\.($_DECBYTE)\.($_DECBYTE)\.($_DECBYTE)\$");
+
+  KdlIPV4(String value, [String? type]) : super(value, type);
 
   static call(KdlValue value, [String type = 'ipv4']) {
     if (!(value is KdlString)) return null;
+    var addr = value.value;
 
-    var addr = InternetAddress(value.value);
-    if (addr.type != InternetAddressType.IPv4) {
-      throw "invalid ipv4 address: ${value.value}";
+    if (!PATTERN.hasMatch(addr)) {
+      throw KdlException("invalid ipv4 address: ${addr}");
     }
 
     return KdlIPV4(addr, type);
@@ -21,14 +34,86 @@ class KdlIPV4 extends KdlIP {
 }
 
 class KdlIPV6 extends KdlIP {
-  KdlIPV6(InternetAddress value, [String? type]) : super(value, type);
+  // IPv6 address format a:b:c:d:e:f:g:h
+  static final _8HEX = r"""
+    (?:[0-9A-Fa-f]{1,4}:){7}
+      [0-9A-Fa-f]{1,4}
+  """
+      .split('\n')
+      .map((v) => v.trim())
+      .join();
+
+  // Compressed IPv6 address format a::b
+  static final _COMPRESSED_HEX = r"""
+    ((?:[0-9A-Fa-f]{1,4}(?::[0-9A-Fa-f]{1,4})*)?)::
+    ((?:[0-9A-Fa-f]{1,4}(?::[0-9A-Fa-f]{1,4})*)?)
+  """
+      .split('\n')
+      .map((v) => v.trim())
+      .join();
+
+  // IPv4 mapped IPv6 address format a:b:c:d:e:f:w.x.y.z
+  static final _6HEX_4DEC = r"""
+    ((?:[0-9A-Fa-f]{1,4}:){6,6})
+    (\d+)\.(\d+)\.(\d+)\.(\d+)
+  """
+      .split('\n')
+      .map((v) => v.trim())
+      .join();
+
+  // Compressed IPv4 mapped IPv6 address format a::b:w.x.y.z
+  static final _COMPRESSED_HEX_4DEC = r"""
+    ((?:[0-9A-Fa-f]{1,4}(?::[0-9A-Fa-f]{1,4})*)?)::
+    ((?:[0-9A-Fa-f]{1,4}:)*)
+    (\d+)\.(\d+)\.(\d+)\.(\d+)
+  """
+      .split('\n')
+      .map((v) => v.trim())
+      .join();
+
+  // IPv6 link local address format fe80:b:c:d:e:f:g:h%em1
+  static final _8HEX_LINK_LOCAL = r"""
+    [Ff][Ee]80
+    (?::[0-9A-Fa-f]{1,4}){7}
+    %[-0-9A-Za-z._~]+
+  """
+      .split('\n')
+      .map((v) => v.trim())
+      .join();
+
+  // Compressed IPv6 link local address format fe80::b%em1
+  static final _COMPRESSED_HEX_LINK_LOCAL = r"""
+    [Ff][Ee]80:
+    (?:
+      ((?:[0-9A-Fa-f]{1,4}(?::[0-9A-Fa-f]{1,4})*)?)::
+      ((?:[0-9A-Fa-f]{1,4}(?::[0-9A-Fa-f]{1,4})*)?)
+      |
+      :((?:[0-9A-Fa-f]{1,4}(?::[0-9A-Fa-f]{1,4})*)?)
+    )?
+    :[0-9A-Fa-f]{1,4}%[-0-9A-Za-z._~]+
+  """
+      .split('\n')
+      .map((v) => v.trim())
+      .join();
+
+  // A composite IPv6 address Regexp.
+  static final PATTERN = RegExp("^${[
+    _8HEX,
+    _COMPRESSED_HEX,
+    _6HEX_4DEC,
+    _COMPRESSED_HEX_4DEC,
+    _8HEX_LINK_LOCAL,
+    _COMPRESSED_HEX_LINK_LOCAL
+  ].map((v) => "(?:$v)").join('|')}\$");
+
+  KdlIPV6(String value, [String? type]) : super(value, type);
 
   static call(KdlValue value, [String type = 'ipv6']) {
     if (!(value is KdlString)) return null;
+    var addr = value.value;
 
-    var addr = InternetAddress(value.value);
-    if (addr.type != InternetAddressType.IPv6) {
-      throw "invalid ipv6 address: ${value.value}";
+    if (!PATTERN.hasMatch(addr)) {
+      throw KdlException("invalid ipv6 address: ${addr}");
     }
 
     return KdlIPV6(addr, type);
--- test/types/ip_test.dart
+++ test/types/ip_test.dart
@@ -1,4 +1,3 @@
-import 'dart:io';
 import 'package:test/test.dart';
 
 import 'package:kdl/src/document.dart';
@@ -6,20 +5,40 @@ import 'package:kdl/src/types/ip.dart';
 
 void main() {
   test('ipv4', () {
-    expect(KdlIPV4.call(KdlString('127.0.0.1')).value,
-      equals(InternetAddress('127.0.0.1')));
+    expect(KdlIPV4.call(KdlString('127.0.0.1')).value, equals('127.0.0.1'));
+    expect(KdlIPV4.call(KdlString('192.168.42.255')).value,
+        equals('192.168.42.255'));
 
-    expect(() => KdlIPV4.call(KdlString('not an ipv4 address')), throwsA(anything));
+    expect(() => KdlIPV4.call(KdlString('not an ipv4 address')),
+        throwsA(anything));
     expect(() => KdlIPV4.call(KdlString('3ffe:505:2::1')), throwsA(anything));
+    expect(() => KdlIPV4.call(KdlString('256.0.0.0')), throwsA(anything));
+    expect(() => KdlIPV4.call(KdlString('312.0.0.0')), throwsA(anything));
   });
 
   test('ipv6', () {
-    expect(KdlIPV6.call(KdlString('::')).value,
-      equals(InternetAddress('::')));
-    expect(KdlIPV6.call(KdlString('3ffe:505:2::1')).value,
-      equals(InternetAddress('3ffe:505:2::1')));
+    final addresses = [
+      'FEDC:BA98:7654:3210:FEDC:BA98:7654:3210',
+      '1080:0:0:0:8:800:200C:417A',
+      '1080:0:0:0:8:800:200C:417A',
+      'FF01:0:0:0:0:0:0:101',
+      '0:0:0:0:0:0:0:1',
+      '0:0:0:0:0:0:0:0',
+      '1080::8:800:200C:417A',
+      'FF01::101',
+      '::1',
+      '::',
+      '0:0:0:0:0:0:13.1.68.3',
+      '0:0:0:0:0:FFFF:129.144.52.38',
+      '::13.1.68.3',
+      '::FFFF:129.144.52.38'
+    ];
+    addresses.forEach((addr) {
+      expect(KdlIPV6.call(KdlString(addr)).value, equals(addr));
+    });
 
-    expect(() => KdlIPV6.call(KdlString('not an ipv4 address')), throwsA(anything));
+    expect(() => KdlIPV6.call(KdlString('not an ipv6 address')),
+        throwsA(anything));
     expect(() => KdlIPV6.call(KdlString('127.0.0.1')), throwsA(anything));
   });
 }
